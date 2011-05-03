024717664b528e04722344faf2c4063faf6c3695
Your Name <james@r0bertson.co.uk>
2011-05-03 12:23:13 +0100

    1st commit

---
+++ lib/rxfhelper.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+# file: rxfhelper.rb
+
+require 'open-uri'
+
+# Read XML File Helper
+#
+class RXFHelper
+
+  attr_reader :to_s
+
+  def initialize(x)
+    @to_s = read x
+  end
+
+  private
+
+  def read(x)   
+    if x.strip[/^</] then
+      x
+    elsif x[/https?:\/\//] then
+      open(x, 'UserAgent' => 'RXFHelper').read  
+    else
+      File.open(s, 'r').read
+    end
+  end
+
+  alias to_xml to_s
+end
---
+++ rxfhelper.gemspec
@@ -0,0 +1,6 @@
+Gem::Specification.new do |s|
+  s.name = 'rxfhelper'
+  s.version = '0.1.0'
+  s.summary = 'rxfhelper'
+  s.files = Dir['lib/**/*.rb']
+end
