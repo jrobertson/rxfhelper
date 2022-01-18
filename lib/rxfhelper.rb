3307a6487251d9ed159bad430cc17bd28d04d019
James Robertson <james@r0bertson.co.uk>
2022-01-18 17:33:04 +0000

    feature: Added FileX#chmod and DirX#glob

--- lib/rxfhelper.rb
+++ lib/rxfhelper.rb
@@ -15,15 +15,24 @@ require 'remote_dwsregistry'
 #        using the Registry feaure to look up objects automatically.
 
 module RXFHelperModule
-  
+
+  class DirX
+
+    def self.chdir(s)  RXFHelper.chdir(s)   end
+    def self.glob(s)   RXFHelper.glob(s)    end
+    def self.mkdir(s)  RXFHelper.mkdir(s)   end
+    def self.pwd()     RXFHelper.pwd()      end
+
+  end
+
   class FileX
 
     def self.chdir(s)  RXFHelper.chdir(s)   end
 
     def self.directory?(filename)
-      
+
       type = self.filetype(filename)
-      
+
       filex = case type
       when :file
         File
@@ -34,15 +43,15 @@ module RXFHelperModule
       end
 
       return nil unless filex
-      
+
       filex.directory? filename
-      
+
     end
-    
+
     def self.exists?(filename)
-      
+
       type = self.filetype(filename)
-      
+
       filex = case type
       when :file
         File
@@ -56,19 +65,19 @@ module RXFHelperModule
       end
 
       return nil unless filex
-      
+
       filex.exists? filename
-      
+
     end
-    
+
     def self.exist?(filename)
       exists? filename
     end
 
     def self.filetype(x)
-      
+
       return :string if x.lines.length > 1
-      
+
       case x
       when /^rse:\/\//
         :rse
@@ -81,26 +90,27 @@ module RXFHelperModule
       when /^file:\/\//
         :file
       else
-        
+
         if File.exists?(x) then
           :file
         else
           :text
         end
-        
+
       end
     end
 
-    def self.cp(s, s2)    RXFHelper.cp(s, s2)      end
-    def self.ls(s)        RXFHelper.ls(s)          end
-    def self.mkdir(s)     RXFHelper.mkdir(s)       end
-    def self.mkdir_p(s)   RXFHelper.mkdir_p(s)     end
-    def self.mv(s, s2)    RXFHelper.mv(s, s2)      end            
-    def self.pwd()        RXFHelper.pwd()          end    
-    def self.read(x)      RXFHelper.read(x).first  end
-    def self.rm(s)        RXFHelper.rm(s)          end
-    def self.write(x, s)  RXFHelper.write(x, s)    end
-    def self.zip(s, a)    RXFHelper.zip(s, a)    end
+    def self.chmod(num, s) RXFHelper.chmod(num, s)     end
+    def self.cp(s, s2)     RXFHelper.cp(s, s2)      end
+    def self.ls(s)         RXFHelper.ls(s)          end
+    def self.mkdir(s)      RXFHelper.mkdir(s)       end
+    def self.mkdir_p(s)    RXFHelper.mkdir_p(s)     end
+    def self.mv(s, s2)     RXFHelper.mv(s, s2)      end
+    def self.pwd()         RXFHelper.pwd()          end
+    def self.read(x)       RXFHelper.read(x).first  end
+    def self.rm(s)         RXFHelper.rm(s)          end
+    def self.write(x, s)   RXFHelper.write(x, s)    end
+    def self.zip(s, a)     RXFHelper.zip(s, a)      end
 
   end
 end
@@ -120,30 +130,43 @@ end
 #
 class RXFHelper
   using ColouredText
-  
+
   @fs = :local
-  
+
   def self.call(s)
-    
+
     if s =~ /=/ then
-      
+
       uri, val = s.split(/=/)
       self.set uri, val
-      
+
     else
-      
+
       self.get s
-      
+
     end
-    
+
   end
-  
+
+  def self.chmod(permissions, s)
+
+    return unless permissions.is_a? Integer
+    return unless s.is_a? String
+
+    if s[/^dfs:\/\//] or @fs == :dfs then
+      DfsFile.chmod permissions, s
+    else
+      FileUtils.chmod permissions, s
+    end
+
+  end
+
   def self.cp(s1, s2)
-    
+
     found = [s1,s2].grep /^\w+:\/\//
-    
+
     if found then
-      
+
       case found.first[/^\w+(?=:\/\/)/].to_sym
 
       when :dfs
@@ -151,18 +174,18 @@ class RXFHelper
       when :ftp
         MyMediaFTP.cp s1, s2
       else
-      
-      end 
-      
+
+      end
+
     else
-      
+
       FileUtils.cp s1, s2
-      
+
     end
   end
-  
+
   def self.chdir(x)
-    
+
     if x[/^file:\/\//] or File.exists?(File.dirname(x)) then
       @fs = :local
       FileUtils.chdir x
@@ -170,32 +193,42 @@ class RXFHelper
       @fs = :dfs
       DfsFile.chdir x
     end
-    
-  end
-  
-  def self.get(x)   
 
-    raise RXFHelperException, 'nil found, expected a string' if x.nil?    
-          
+  end
+
+  def self.get(x)
+
+    raise RXFHelperException, 'nil found, expected a string' if x.nil?
+
     if x[/^rse:\/\//] then
-      
+
       RSC.new.get x
-      
+
     elsif x[/^reg:\/\//] then
-        
+
       r = DRbRegClient.new.get(x)
       r.is_a?(Rexle::Element::Value) ? r.to_s : r
-      
+
     else
       [x, :unknown]
     end
 
   end
-  
+
+  def self.glob(s)
+
+    if s[/^dfs:\/\//] then
+      DfsFile.glob(s)
+    else
+      Dir.glob(s)
+    end
+
+  end
+
   def self.ls(x='*')
-    
+
     return Dir[x] if File.exists?(File.dirname(x))
-    
+
     case x[/^\w+(?=:\/\/)/].to_sym
     when :file
       Dir[x]
@@ -204,127 +237,127 @@ class RXFHelper
     when :ftp
       MyMediaFTP.ls x
     else
-    
-    end       
-    
+
+    end
+
   end
-  
+
   def self.mkdir(x)
-    
+
     if x[/^file:\/\//] or File.exists?(File.dirname(x)) then
       FileUtils.mkdir x
     elsif x[/^dfs:\/\//]
       DfsFile.mkdir x
     end
-    
+
   end
-  
+
   def self.mkdir_p(x)
-    
+
     if x[/^dfs:\/\//] or @fs == :dfs then
       DfsFile.mkdir_p x
     else
-      FileUtils.mkdir_p x      
+      FileUtils.mkdir_p x
     end
-    
+
   end
-  
+
   def self.mv(s1, s2)
     DfsFile.mv(s1, s2)
-  end  
-  
+  end
+
   # used by self.read
-  #  
+  #
   def self.objectize(contents)
-    
+
     doctype = contents.lines.first[/(?<=^<\?)\w+/]
     reg = RemoteDwsRegistry.new domain: 'reg.lookup', port: '9292'
     r = reg.get_key 'hkey_gems/doctype/' + doctype
-        
+
     return contents unless r
 
     require r.text('require')
-    
+
     obj = Object.const_get(r.text('class')).new
     obj.import contents
-    obj    
+    obj
   end
-  
+
   def self.pwd()
-    
+
     DfsFile.pwd
-    
-  end  
-  
-  def self.read(x, h={})   
-    
+
+  end
+
+  def self.read(x, h={})
+
     opt = {debug: false, auto: false}.merge(h)
-    
+
     debug = opt[:debug]
 
     puts 'x: ' + x.inspect if opt[:debug]
     raise RXFHelperException, 'nil found, expected a string' if x.nil?
-    
+
     if x.class.to_s =~ /Rexle$/ then
-      
+
       [x.xml, :rexle]
-      
+
     elsif x.strip[/^<(\?xml|[^\?])/] then
-      
+
       [x, :xml]
-      
+
     elsif x.lines.length == 1 then
-      
+
       puts 'x.lines == 1'.info if debug
-      
+
       if x[/^https?:\/\//] then
-        
+
         puts 'before GPDRequest'.info if debug
-        
+
         r = GPDRequest.new(opt[:username], opt[:password]).get(x)
-        
+
         case r.code
-        when '404'          
+        when '404'
           raise(RXFHelperException, "404 %s not found" % x)
-        when '401'          
-          raise(RXFHelperException, "401 %s unauthorized access" % x)        
+        when '401'
+          raise(RXFHelperException, "401 %s unauthorized access" % x)
         end
-        
+
         obj = opt[:auto] ? objectize(r.body) :   r.body
-        
+
         [obj, :url]
-        
+
       elsif  x[/^dfs:\/\//] then
-        
+
         r = DfsFile.read(x)
-        [opt[:auto] ? objectize(r) : r, :dfs]        
-        
+        [opt[:auto] ? objectize(r) : r, :dfs]
+
       elsif  x[/^ftp:\/\//] then
-        
-        [MyMediaFTP.read(x), :ftp]                
-                
+
+        [MyMediaFTP.read(x), :ftp]
+
       elsif x[/^rse:\/\//] then
-        
+
          [RSC.new.get(x), :rse]
-         
+
       elsif x[/^reg:\/\//] then
-        
-        r = DRbRegClient.new.get(x)        
+
+        r = DRbRegClient.new.get(x)
         [r.is_a?(Rexle::Element::Value) ? r.to_s : r, :reg]
-         
+
       elsif x[/^file:\/\//] or File.exists?(x) then
-        
+
         puts 'RXFHelper.read before File.read' if opt[:debug]
         contents = File.read(File.expand_path(x.sub(%r{^file://}, '')))
-        
+
         puts 'contents2: ' + contents.inspect if opt[:debug]
-        
+
         puts 'opt: ' + opt.inspect if opt[:debug]
-        
+
         obj = opt[:auto] ? objectize(contents) :   contents
-        
+
         [obj, :file]
-        
+
       elsif x =~ /\s/
         [x, :text]
       elsif DfsFile.exists?(x)
@@ -332,73 +365,73 @@ class RXFHelper
       else
         [x, :unknown]
       end
-      
+
     else
 
       [x, :unknown]
     end
   end
-  
+
   def self.rm(filename)
-        
+
     case filename[/^\w+(?=:\/\/)/].to_sym
     when :dfs
       DfsFile.rm filename
     when :ftp
-      MyMediaFTP.rm filename      
+      MyMediaFTP.rm filename
     else
-    
-    end    
-    
+
+    end
+
   end
-  
+
   def self.write(location, s=nil)
-    
+
     case location
     when /^dfs:\/\//
-      
+
       DfsFile.write location, s
-      
+
     when  /^ftp:\/\// then
-        
+
       MyMediaFTP.write location, s
-      
+
     when /^rse:\/\//
-      
+
       RSC.new.post(location, s)
 
     when /^reg:\/\//
-        
+
       DRbRegClient.new.set(location, s)
-      
+
     else
-      
+
       if DfsFile.exists?(File.dirname(location)) then
         DfsFile.write location, s
       else
         File.write(location, s)
       end
-      
+
     end
-    
+
   end
-  
+
   def self.writeable?(source)
 
     return false if source.lines.length > 1
-    
+
     if not source =~ /:\/\// then
-      
+
       return true if File.exists? source
-      
+
     else
-      
+
       return true if source =~ /^dfs:/
-      
+
     end
-    
+
     return false
-  end  
+  end
 
   def self.absolute_url(page_url, item_location)
 
@@ -413,42 +446,42 @@ class RXFHelper
       else
         File.join page_url[/.*\//], item_location
     end
-  end  
-  
-  def self.post(uri, x=nil)   
+  end
 
-    raise RXFHelperException, 'nil found, expected a string' if uri.nil?    
-          
+  def self.post(uri, x=nil)
+
+    raise RXFHelperException, 'nil found, expected a string' if uri.nil?
+
     if uri[/^rse:\/\//] then
       RSC.new.post uri, x
-      
+
     elsif uri[/^reg:\/\//]
-        
-      DRbRegClient.new.set(uri, x)      
+
+      DRbRegClient.new.set(uri, x)
     else
       [uri, :unknown]
     end
 
   end
-  
-  def self.set(uri, x=nil)   
 
-    raise RXFHelperException, 'nil found, expected a string' if uri.nil?    
+  def self.set(uri, x=nil)
+
+    raise RXFHelperException, 'nil found, expected a string' if uri.nil?
     puts 'uri: ' + uri.inspect
-          
+
     if uri[/^rse:\/\//] then
       RSC.new.post uri, x
-      
+
     elsif uri[/^reg:\/\//]
-        
-      DRbRegClient.new.set(uri, x)      
+
+      DRbRegClient.new.set(uri, x)
     else
       [uri, :unknown]
     end
 
-  end  
+  end
 
   def self.zip(filename, a)
     DfsFile.zip(filename, a)
-  end    
+  end
 end
--- rxfhelper.gemspec
+++ rxfhelper.gemspec
@@ -1,19 +1,19 @@
 Gem::Specification.new do |s|
   s.name = 'rxfhelper'
-  s.version = '1.1.3'
+  s.version = '1.1.4'
   s.summary = 'Helpful library for primarily reading the contents of a ' + 
       'file either from an HTTP address, local file, or DRb file system.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/rxfhelper.rb']
-  s.add_runtime_dependency('rsc', '~> 0.4', '>=0.4.4')
-  s.add_runtime_dependency('drb_fileclient', '~> 0.5', '>=0.5.2')
-  s.add_runtime_dependency('mymedia_ftp', '~> 0.3', '>=0.3.0')
+  s.add_runtime_dependency('rsc', '~> 0.4', '>=0.4.5')
+  s.add_runtime_dependency('drb_fileclient', '~> 0.6', '>=0.6.1')
+  s.add_runtime_dependency('mymedia_ftp', '~> 0.3', '>=0.3.3')
   s.add_runtime_dependency('remote_dwsregistry', '~> 0.4', '>=0.4.1')
   s.add_runtime_dependency('drb_reg_client', '~> 0.2', '>=0.2.0')
   s.signing_key = '../privatekeys/rxfhelper.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/rxfhelper'
   s.required_ruby_version = '>= 2.1.2'
 end
